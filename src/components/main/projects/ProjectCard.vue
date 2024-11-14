75059b78b9167ae4a3ee4306e87af38266656b23
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-14 15:51:59 +0100

    created a new component that represents the single card of the project and passed the data of the same via props

---
+++ src/components/main/projects/ProjectCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+    },
+    props: {
+        projectId: {
+            type: Number,
+            required: true,
+        },
+        projectName: {
+            type: String,
+            required: true,
+        },
+        projectDescription: {
+            type: String,
+            required: true,
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ projectName }}</h5>
+            <h6>{{ projectId }}</h6>
+            <p class="card-text">{{ projectDescription }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
