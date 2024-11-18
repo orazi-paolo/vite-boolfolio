b518b836e0b0f2918997c221f3100afe22a4ae17
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 16:25:14 +0100

    created footer layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,14 +10,42 @@ export default {
 </script>
 
 <template>
-  <nav>
-    <ul class="d-flex justify-content-center list-unstyled">
-      <li class="me-3">link 1</li>
-      <li class="me-3">link 2</li>
-      <li class="me-3">link 3</li>
-      <li>link 4</li>
-    </ul>
-  </nav>
-</template>
+  <footer class="bg-dark text-white py-4">
+    <div class="container text-center">
+      <h3 class="fw-bold mb-3">
+        Social
+      </h3>
+      <div class="mb-5 d-flex justify-content-center">
+        <a href="https://github.com/orazi-paolo" class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fab fa-github me-2"></i> Github
+        </a>
+        <a href="#" class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fab fa-linkedin me-2"></i> Linkedin
+        </a>
+        <a href="#" class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fab fa-instagram me-2"></i> Instagram
+        </a>
+      </div>
+      <h3 class="fw-bold mb-3">
+        Best framework used
+      </h3>
+      <div class="mb-3 d-flex justify-content-center">
+        <a href="https://vuejs.org/guide/introduction.html"
+          class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fab fa-vuejs me-2"></i> Vue.js
+        </a>
+        <a href="https://router.vuejs.org/" class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fas fa-route me-2"></i> Vue Router
+        </a>
+        <a href="https://laravel.com/docs/10.x/readme" class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fab fa-laravel me-2"></i> Laravel
+        </a>
+        <a href="https://getbootstrap.com/" class="btn btn-outline-light mx-2 d-flex align-items-center">
+          <i class="fab fa-bootstrap me-2"></i> Bootstrap
+        </a>
+      </div>
 
-<style scoped></style>
+      <p>&copy; 2024 Paolo Orazi. All Rights Reserved.</p>
+    </div>
+  </footer>
+</template>
