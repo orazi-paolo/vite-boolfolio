53d8991eb81cbe5faa5132a520e7d5223ee51c2b
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 17:38:46 +0100

    created the "show" with vue router by making a new api call

---
+++ src/components/projects/ProjectShow.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            apiUrl: 'http://127.0.0.1:8000/api/projects/',
+            singleProject: {},
+        }
+    },
+    methods: {
+        getSingleProject() {
+            axios.get(`${this.apiUrl}${this.$route.params.id}`)
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.singleProject = response.data.results;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+        }
+    },
+    created() {
+        this.getSingleProject();
+    }
+}
+</script>
+
+<template>
+    <div class="card shadow-sm border-0 text-white bg-dark mb-3">
+        <div class="card-header text-center fw-bold bg-secondary">
+            {{ singleProject.id }} -- {{ singleProject.name }}
+        </div>
+        <div class="card-body">
+            <p class="card-text text-center">{{ singleProject.description }}</p>
+        </div>
+    </div>
+</template>
--- src/js/router.js
+++ src/js/router.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import AppHome from '../pages/AppHome.vue';
 import AppAbout from '../pages/AppAbout.vue';
 import AppProjects from '../pages/AppProjects.vue';
+import ProjectShow from '../components/projects/ProjectShow.vue';
 
 const routes = [
   { path: '/', component: AppHome, name: 'home' },
   { path: '/about', component: AppAbout, name: 'about' },
   { path: '/projects', component: AppProjects, name: 'projects' },
+  { path: '/projects/:id', component: ProjectShow, name: 'projects.show' },
 ]
 
 const router = createRouter({
