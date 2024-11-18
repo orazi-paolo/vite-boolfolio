f3eacb207af5ff53f5032269b5bafa7154fbdf01
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 15:13:21 +0100

    create About Home and Projects

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  name: 'AppHome',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<template>
+  <h2 class="text-center">
+    Home
+  </h2>
+
+</template>
+
+<style scoped></style>
---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,25 @@
+<script>
+import ProjectsList from '../components/projects/ProjectsList.vue';
+
+export default {
+  name: 'AppPosts',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    ProjectsList
+  }
+}
+</script>
+
+<template>
+  <h2 class="text-center">
+    Projects
+  </h2>
+  <ProjectsList />
+
+</template>
+
+<style scoped></style>
