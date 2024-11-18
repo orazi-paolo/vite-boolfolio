f3230ba9eb82de4797418388d9b6ac547d218790
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 15:13:00 +0100

    create AppHeader and AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'AppFooter',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <nav>
+    <ul class="d-flex justify-content-center list-unstyled">
+      <li class="me-3">link 1</li>
+      <li class="me-3">link 2</li>
+      <li class="me-3">link 3</li>
+      <li>link 4</li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link active" aria-current="page"
+                                :to="{ name: 'home' }">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{ name: 'about' }">About</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{ name: 'projects' }">Projects</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped></style>
