8107dd8eb836a6644d87ad9ec51355b3cd2027bc
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 15:13:45 +0100

    update App.vue with router view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script>
-import AppMain from './components/main/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
 export default {
   data() {
     return {
-      
+
     }
   },
   components: {
-    AppMain,
+    AppFooter,
+    AppHeader
   }
 }
 </script>
 
 <template>
-<AppMain />
+  <AppHeader />
+  <main>
+    <RouterView />
+  </main>
+  <AppFooter />
 </template>
 
 <style lang="scss">
