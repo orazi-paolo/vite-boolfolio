06eb72f81a468bf7831031705548926317bf5b5e
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 16:24:53 +0100

    installed and imported font awesome

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import { router } from './router'
 import App from '../App.vue'
 
