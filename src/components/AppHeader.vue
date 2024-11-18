2c218037a34714797d1ddbb2006b792ff7c8620d
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-18 15:40:26 +0100

    improved style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,40 +1,58 @@
 <script>
 export default {
     name: 'AppHeader',
-    data() {
-        return {
-
-        }
-    }
 }
 </script>
 
 <template>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <router-link class="nav-link active" aria-current="page"
-                                :to="{ name: 'home' }">Home</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" :to="{ name: 'about' }">About</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" :to="{ name: 'projects' }">Projects</router-link>
-                        </li>
-                    </ul>
-                </div>
+    <header class="mb-3">
+        <nav class="navbar navbar-expand bg-dark navbar-dark">
+            <div class="container">
+                <a class="navbar-brand fw-bold" href="#">Paolo's portfolio</a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <router-link class="nav-link" :class="{ active: $route.name === 'home' }"
+                            :to="{ name: 'home' }"> Home </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :class="{ active: $route.name === 'about' }"
+                            :to="{ name: 'about' }">
+                            About
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :class="{ active: $route.name === 'projects' }"
+                            :to="{ name: 'projects' }">
+                            Projects
+                        </router-link>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
 </template>
 
-<style scoped></style>
+
+
+<style scoped>
+.navbar {
+    background-color: #343a40;
+}
+
+.navbar-brand {
+    color: white;
+    font-size: 1.5rem;
+    text-transform: uppercase;
+}
+
+.nav-link {
+    color: lightgray;
+    transition: color 0.3s, font-weight 0.3s;
+}
+
+.nav-link:hover,
+.nav-link.active {
+    color: #ffffff;
+    font-weight: bold;
+}
+</style>
